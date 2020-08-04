7fbcd3676bec28b9cbff519206acb0ddfe7849c5
RgrMz <63113292+RgrMz@users.noreply.github.com>
2020-08-04 19:35:35 +0200

    MyInfo component added

---
+++ src/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyInfo() {
+    return(
+        <div classname="info">
+            <h2>A little of myself</h2>
+            <p>HI all, my name is Ruben Grane Muñoz. I'm 20 and currently I'm trying to learn React until I want to shoot my own balls!</p>
+            <ol>
+                <li>I want to poo</li>
+                <li>Also I want to boom my head</li>
+                <li>And finally my balls</li>
+            </ol>
+        </div>
+    );
+}
+
+export default MyInfo;
+
